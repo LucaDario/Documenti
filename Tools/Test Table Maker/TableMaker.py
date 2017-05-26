9db2e03e63dd9d16b4e5f292abae9396ad5eaffc
Manu@ares-xps15 <manuel.turetta94@gmail.com>
2017-05-26 17:41:08 +0200

    Tools - Aggiornato lo script per la generazione delle tabelle relative ai test al fine di separare esito e implementazione dei vari test

--- Tools/Test Table Maker/TableMaker.py
+++ Tools/Test Table Maker/TableMaker.py
@@ -16,18 +16,33 @@ if __name__ == '__main__':
     for title, section in test_tracker:
         print("Inizio sezione " + title)
         camel_code_title = remove_accents(title.title().replace(" ", "") + ".tex")
+        camel_code_results_title = "Esiti" + camel_code_title
         section_file = open(camel_code_title, 'w')
+        results_file = open(camel_code_results_title, 'w')
+
         section_file.write("\\begin{center}\n")
-        section_file.write("\t\\begin{longtable}{|c|>{\centering}m{10cm}|c|c|}\\hline\n")
-        section_file.write("\t\tId & Descrizione & Stato & Esito\\\\ \\hline\n")
+        section_file.write("\t\\begin{longtable}{|c|>{\centering}m{10cm}|c|}\\hline\n")
+        section_file.write("\t\tId & Descrizione & Stato\\\\ \\hline\n")
+
+        results_file.write("\\begin{center}\n")
+        results_file.write("\t\\begin{longtable}{|c|>{\centering}m{10cm}|c|}\\hline\n")
+        results_file.write("\t\tId & Descrizione & Esito\\\\ \\hline\n")
+
         for id, test in section:
             implemented_tex = '\\textcolor{Orange}{Non implementato}'
             if test['implemented']:
                 implemented_tex = '\\textcolor{Green}{Implementato}'
+            section_file.write("\t\t" + id + " & " + test['description'] + " & " + implemented_tex + "\\\\ \\hline\n")
+
             result_text = '\\textcolor{Orange}{Non superato}'
             if test['passed']:
                 result_text = '\\textcolor{Green}{Superato}'
-            section_file.write("\t\t" + id + " & " + test['description'] + " & " + implemented_tex + " & " + result_text + " \\\\ \\hline\n")
+            results_file.write("\t\t" + id + " & " + test['description'] + " & " + result_text + "\\\\ \\hline\n")
+
         section_file.write("\t\\end{longtable}\n")
         section_file.write("\\end{center}\n")
         section_file.close()
+
+        results_file.write("\t\\end{longtable}\n")
+        results_file.write("\\end{center}\n")
+        results_file.close()
